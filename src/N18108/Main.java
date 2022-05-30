0b9b0239f120480608b6a8b499737a3ee4794b74
Ck_SsoH <Ck_SsoH@DESKTOP-VJ06FMD>
2022-05-30 21:32:22 +0900

    [차소현] 백준 단계별 1단계 완료

---
+++ src/N18108/Main.java
@@ -0,0 +1,12 @@
+package N18108;
+
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = 543;
+		System.out.println(a - b);
+	}
+}
