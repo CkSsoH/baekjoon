ad705ec55a2a60697ecc10c63e7d6167a42f2eb6
Ck_SsoH <Ck_SsoH@DESKTOP-VJ06FMD>
2022-06-07 02:31:15 +0900

    [차소현] 백준 4단계 2번까지 커밋

--- src/N2562/Main.java
+++ src/N2562/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int max = 0;
+		int maxLine =0;
 		ArrayList<Integer> list = new ArrayList<>();
 		
 		for(int i = 0; i<9; i++) {
@@ -13,15 +14,17 @@ public class Main {
 			list.add(n);
 		}
 		/* System.out.println(list); */
-		
-		for(Integer x : list) {
-			if(max < x) {
-				max = x;
+		int x;
+		for(x =0; x<list.size(); x++) {
+			if(max < list.get(x)) {
+				max = list.get(x);
+			}
+			if(list.get(x) == max) {
+				maxLine=x;
 			}
 		}
 		System.out.println(max);
-		
-		System.out.println();
+		System.out.println(maxLine+1);
 	}
 
 }
--- src/N2577/Main.java
+++ src/N2577/Main.java
@@ -1,9 +1,16 @@
 package N2577;
 
+import java.util.*;
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b, c;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		int x = a*b*c;
+		ArrayList<Integer> arr = new ArrayList<>();
 		
-
 	}
 
 }
